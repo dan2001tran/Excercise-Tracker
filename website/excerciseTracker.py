96d913c91cc373d370f63b773a18b8f4836cf952
Dan Tran <dan2001tran@gmail.com>
2021-09-15 00:30:07 -0400

    Daily updates

--- website/excerciseTracker.py
+++ website/excerciseTracker.py
@@ -29,6 +29,7 @@ def add():
         messageGateExcercise = False
         messageGateRPE = False
         messageGateWeight = False
+        messageGateReps = False
 
         userInput = False
 
@@ -40,6 +41,10 @@ def add():
         if not weight:
              messageGateWeight = True
              userInput = True
+        reps = request.form.get('Rep-Range')
+        if not reps:
+             messageGateReps = True
+             userInput = True
         rpe3 = request.form.get('RPE3')
         rpe2 = request.form.get('RPE2')
         rpe1 = request.form.get('RPE1')
@@ -48,11 +53,11 @@ def add():
             messageGateRPE = True
             userInput = True
         if userInput:
-            return render_template("add.html", messageGateWeight = messageGateWeight, messageGateExcercise = messageGateExcercise, messageGateRPE = messageGateRPE)
+            return render_template("add.html", messageGateWeight = messageGateWeight, messageGateExcercise = messageGateExcercise, messageGateReps = messageGateReps, messageGateRPE = messageGateRPE)
         
         db = sqlite3.connect('excercise.db')
         cur = db.cursor()  
-        cur.execute("INSERT INTO lifts (excercise, weight, RPE3, RPE2, RPE1) VALUES (?, ?, ?, ?, ?)", (excercise, weight, rpe3, rpe2, rpe1))
+        cur.execute("INSERT INTO lifts (excercise, weight, reps, RPE3, RPE2, RPE1) VALUES (?, ?, ?, ?, ?, ?)", (excercise.lower(), weight, reps, rpe3, rpe2, rpe1))
         
         db.commit()
         cur.close()
@@ -65,6 +70,64 @@ def add():
 @excerciseTracker.route('/delete', methods= ['GET', 'POST'])
 def delete():
     if request.method == "POST":
-        return render_template("delete.html", formSent = True)
+        messageGateExcercise = False
+        userInput = False
+        excercise = request.form.get('Excercise')
+        excercise.lower();
+        if not excercise:
+             messageGateExcercise = True
+             userInput = True
+
+        if userInput:
+            return render_template("add.html", messageGateExcercise = messageGateExcercise)
+        
+        db = sqlite3.connect('excercise.db')
+        cur = db.cursor()  
+        cur.execute("DELETE FROM lifts WHERE excercise =?", (excercise,))
+        
+        db.commit()
+        cur.close()
+        db.close()
+        return redirect("/")
+    
     else:
-        return render_template("delete.html", message = "this is a get")
+
+        return render_template("delete.html")
+
+@excerciseTracker.route('/update', methods= ['GET', 'POST'])
+def update():
+    if request.method == "POST":
+
+        messageGateExcercise = False
+        messageGateUpdate = False
+
+        userInput = False
+
+        excercise = request.form.get('Excercise')
+        if not excercise:
+             messageGateExcercise = True
+             userInput = True
+        
+        weight = request.form.get('Weight')
+        reps = request.form.get('Rep-Range')
+        rpe3 = request.form.get('RPE3')
+        rpe2 = request.form.get('RPE2')
+        rpe1 = request.form.get('RPE1')
+
+        if not weight and not reps and not rpe1 and not rpe2 and not rpe3:
+            messageGateUpdate = True
+            userInput = True
+        if userInput:
+            return render_template("update.html", messageGateExcercise = messageGateExcercise, messageGateUpdate = messageGateUpdate)
+        
+        db = sqlite3.connect('excercise.db')
+        cur = db.cursor()  
+        cur.execute("INSERT INTO lifts (excercise, weight, reps, RPE3, RPE2, RPE1) VALUES (?, ?, ?, ?, ?, ?)", (excercise.lower(), weight, reps, rpe3, rpe2, rpe1))
+        
+        db.commit()
+        cur.close()
+        db.close()
+        return redirect("/")
+    else:
+
+        return render_template("update.html", messageGate = False)
